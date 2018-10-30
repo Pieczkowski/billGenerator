f8f6e7f7ea56fd90543f0d1dfebe10f2e4613101
Pieczkowski <pieczkowski.t@gmail.com>
2018-10-30 14:14:44 +0100

    Add testValidPriceForMultipleItems

--- src/test/java/org/tomasz/billgenerator/BillGeneratorTest.java
+++ src/test/java/org/tomasz/billgenerator/BillGeneratorTest.java
@@ -23,4 +23,21 @@ class BillGeneratorTest {
         assertEquals(expected, result);
     }
 
+    @Test
+    void testValidPriceForMultipleItems(){
+        BillGenerator billGenerator = new BillGenerator();
+        List<Integer> basket = new ArrayList<>();
+        int barcodeForBeer = 1001;
+        int barcodeChocolate = 3401;
+        basket.add(barcodeForBeer);
+        basket.add(barcodeForBeer);
+        basket.add(barcodeForBeer);
+        basket.add(barcodeChocolate);
+        BigDecimal expected = BigDecimal.valueOf(6.35);
+
+        BigDecimal result = billGenerator.getBill(basket);
+
+        assertEquals(expected, result);
+    }
+
 }
